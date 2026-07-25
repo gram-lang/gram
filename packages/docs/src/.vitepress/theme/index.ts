f420dbf1da9d17f65065eaf1e13db281393017f3
abiwab <abiwab@gmail.com>
2026-07-25 21:29:50 +0200

    fix(docs): register Mermaid component explicitly in VitePress theme

--- packages/docs/src/.vitepress/theme/index.ts
+++ packages/docs/src/.vitepress/theme/index.ts
@@ -1,5 +1,7 @@
 import { defineAsyncComponent } from "vue";
 import DefaultTheme from "vitepress/theme";
+import Mermaid from "vitepress-mermaid-viewer/Mermaid";
+import "vitepress-mermaid-viewer/style.css";
 import "./style.css";
 import "@gram-lang/renderer/gram.css";
 import "@gram-lang/renderer/gantt.css";
@@ -7,6 +9,7 @@ import "@gram-lang/renderer/gantt.css";
 export default {
 	extends: DefaultTheme,
 	enhanceApp({ app }) {
+		app.component("Mermaid", Mermaid);
 		app.component(
 			"GramPlayground",
 			defineAsyncComponent(
